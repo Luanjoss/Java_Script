3b12c1e1ed8e7214a597f23b3b5add7b3b2454cd
Luanjoss <luanmicro890@gmail.com>
2022-02-13 22:51:22 -0300

    teste

---
+++ Testes/teste.js
@@ -0,0 +1,10 @@
+function Maiornumero (a, b) {
+
+    if (a > b) {
+        console.log("O primeiro numero é maior", a)
+    } else {
+        console.log("O segundo numero é maior", b)
+    }
+}
+
+console.log(Maiornumero(5,11));
